0f38352b68bb5c5cb043c446abcfa3a8abeed15f
anil kumar <anilkumarmalluvalasa64@gmail.com>
2026-06-15 23:41:42 +0530

    Fix Template path

--- BloodDonation/settings.py
+++ BloodDonation/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "BloodDonation.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "Donation" / "Templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
